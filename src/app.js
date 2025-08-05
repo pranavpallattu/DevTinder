f8efa7094532fd9a728c18fcc6038bd3e8438a6f
pranavpallattu <pranavpspallattu@gmail.com>
2025-08-05 11:48:17 +0530

    exploring routes

--- src/app.js
+++ src/app.js
@@ -3,17 +3,54 @@ const express=require("express")
 // creates an express application
 const app=express()   
 
-app.use("/text",function(req,res){
-    res.send("Hello from server")
+// app.use("/user",(req,res)=>{
+//     res.send("app.use called")
+// })
+
+app.get("/user",(req,res)=>{
+    res.send({Firstname:"Thala",LastName: "Dhoni"})
 })
 
-app.use("/getname",function(req,res){
-    res.send("Name is anonymou7")
+app.post("/user",(req,res)=>{
+    res.send("User data saved successfully")
 })
 
-app.use("/savename",function(req,res){
-    res.send("save name of the author as thala for a reason")
+
+app.delete("/user",(req,res)=>{
+    res.send("deleted user data saved successfully")
 })
+
+
+// app.use("/",function(req,res){
+//     res.send("Namaste Pranav")
+// })
+
+// app.use("/text/77",(req,res)=>{
+//     res.send("MAGIC MAGIC MAGIC")
+// })
+
+
+// app.use("/text",function(req,res){
+//     res.send("Hello from server")
+// })
+
+// app.use("/text/77",(req,res)=>{
+//     res.send("MAGIC MAGIC MAGIC")
+// })
+
+// app.use("/getname",function(req,res){
+//     res.send("Name is anonymou7")
+// })
+
+// app.use("/savename",function(req,res){
+//     res.send("save name of the author as thala for a reason")
+// })
+
+
+// app.use("/",function(req,res){
+//     res.send("Namaste Pranav")
+// })
+
 
 
 app.listen(7777,()=>{
